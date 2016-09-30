0db1774ebe7e4b83a3a8a5b4cbdca225ee08f27d
Fred Sladkey <fsladkey@gmail.com>
2016-09-29 21:43:40 -0400

    Remove unneeded argument in applyMiddlware.

--- applyMiddlware.js
+++ applyMiddlware.js
@@ -1,6 +1,6 @@
 export default function applyMiddleware(...middlewares) {
   return (createStore) => (reducer, preloadedState, enhancer) => {
-    const store = createStore(reducer, preloadedState, enhancer);
+    const store = createStore(reducer, preloadedState);
     let { getState, dispatch } = store;
 
     middlewares = middlewares.map(middleware => {
